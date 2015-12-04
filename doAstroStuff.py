2492c7637d1386ffddfbbf949ae09937ba5cfc2c
Laura Maixner <greenfantasi@hotmail.com>
2015-12-04 14:57:06 -0600

    fixed PEP 8 errors

--- doAstroStuff.py
+++ doAstroStuff.py
@@ -5,27 +5,27 @@ from SortGiantPileofSpreadsheets import *
 sourceFilesLocation = r'C:\Users\ShakAttack\Desktop\Everything\Class\2015Fall\Phys300\2015_09_25_Reduced'
 
 # Where to put intermediate spreadsheets
-#full path or folder in current directory
+# full path or folder in current directory
 pileFileLocation = r'C:\Users\ShakAttack\Desktop\Everything\Class\2015Fall\Phys300\garbage'
 
 # For Filter Grouped sheets
 objectName = "M71"
 filteredFileLocation = r'C:\Users\ShakAttack\Desktop\Everything\Class\2015Fall\Phys300\output'
-filtersToUse = ['I','R','V','B']
+filtersToUse = ['I', 'R', 'V', 'B']
 targetDirectory = 'output'
 
-#files = loadFiles(sourceFilesLocation)
+files = loadFiles(sourceFilesLocation)
 
 print("Files are loaded... ")
 
-#writeTables(files,pileFileLocation) 
-#location/name of target directory can be set but has default
-#inner/outer radii, gain, and read noise can be sent but have defaults
+writeTables(files, pileFileLocation)
+# location/name of target directory can be set but has default
+# inner/outer radii, gain, and read noise can be sent but have defaults
 
 print("You are burried in a giant pile of spreadsheets... ")
 
-GroupByFilter(filteredFileLocation,objectName,filtersToUse,targetDirectory)
-#location/name of target directory can be set but has default
-#can be sent a list of filters to go through but defaults to I,R,V,B filters
+GroupByFilter(filteredFileLocation, objectName, filtersToUse, targetDirectory)
+# location/name of target directory can be set but has default
+# can be sent a list of filters to go through but defaults to I,R,V,B filters
 
 print("They are now grouped by filter... ")
