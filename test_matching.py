6f0f568dfa4de24cc80c16e3c1ba2a211cb0905b
Laura Maixner <greenfantasi@hotmail.com>
2016-02-12 21:12:14 -0600

    File to test SortGiantPileofSpreadsheets.py without painfully sifting through spreadsheets.  Well it eventually will.

---
+++ test_matching.py
@@ -0,0 +1,16 @@
+def test_catalogue_with_self():
+	"""
+read in one of the test files
+compare file with itself to make sure the data nums are getting
+assigned correctly
+expect result that each object matches itself
+"""
+
+
+def test_catalog_with_self_offset():
+	"""
+read one of thest files
+make a new test file from it by adding one degree to each RA or Dec
+then if you compare the test file to the new one there should be no
+matches if the cutoff is short enough
+"""
