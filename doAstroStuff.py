3c0d078566244196b2c4c3a12931d4b37baf25e8
Laura Maixner <greenfantasi@hotmail.com>
2016-05-20 12:11:13 -0500

    updated version that properly runs the changed modules (should have put in an updated version with each of the previous commits but life...)

--- doAstroStuff.py
+++ doAstroStuff.py
@@ -1,39 +1,47 @@
 from GetGiantPileofSpreadsheets import *
 from SortGiantPileofSpreadsheets import *
 from AvgFlux import *
+from CMD import *
+import os
 
 # Location of Source Images
 sourceFilesLocation = r'C:\Users\ShakAttack\Desktop\Everything\Class\2015Fall\Phys300\2015_09_25_Reduced'
 
 # Where to put intermediate spreadsheets
 # full path or folder in current directory
-pileFileLocation = r'garbage'
+pileFileLocation = r'output'
 
 # For Filter Grouped sheets
 objectName = "M71"
-filteredFileLocation = r'garbage'
+filteredFileLocation = r'Sorted'
 filtersToUse = ['I', 'R', 'V', 'B']
-targetDirectory = 'garbage'
+AveragedLocation = r'Averaged'
+AvgLocAndFilt = r'C:\Users\ShakAttack\Desktop\Everything\Class\Git\M71\Sorted\*Filt*.csv'
 
-# Location and glob mask for the averaging function, would like to split
-AvgLocAndFilt = r'C:\Users\ShakAttack\Desktop\Everything\Class\Git\garbage\*Filt*.csv'
+# CMD
+LongWavelength = r'*I*.csv'
+ShortWavelength = r'*R*.csv'
+
 
 files = load_files(sourceFilesLocation)
 
 print("Files are loaded... ")
 
-write_tables(files, pileFileLocation, objectName)
-# location/name of target directory can be set but has default
+next_loc = write_tables(files, pileFileLocation, objectName)
+# location/name of targetdirectory can be set but has default
 # inner/outer radii, gain, and read noise can be sent but have defaults
 
 print("You are burried in a giant pile of spreadsheets... ")
 
-group_by_filter(filteredFileLocation, objectName, filtersToUse, targetDirectory)
+next_loc = group_by_filter(next_loc, objectName, filtersToUse, filteredFileLocation, objectName)
 # location/name of target directory can be set but has default
 # can be sent a list of filters to go through but defaults to I,R,V,B filters
 
 print("They are now grouped by filter... ")
 
-avg_flux(AvgLocAndFilt,targetDirectory)
+
+next_loc = avg_flux(os.path.join(os.path.join(r'C:\Users\ShakAttack\Desktop\Everything\Class\Git', next_loc), '*Filt*'),AveragedLocation, objectName)
 
 print("Filters have been averaged... ")
+
+make_CMD(os.path.join(os.path.join(next_loc, ShortWavelength)), os.path.join(next_loc, LongWavelength))
